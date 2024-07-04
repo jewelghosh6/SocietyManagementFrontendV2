f0e29e78522bfe15d8ddfd1e551d15f3174c0c0b
jewelghosh6 <jewelghosh611@gmail.com>
2024-07-04 21:32:08 +0530

    changed api url in env files

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,7 +1,6 @@
 const config = {
-  // API_URL: "https://society-management-backend-v2.onrender.com/api",
-  API_URL: "https://tfk9pkp6-3001.inc1.devtunnels.ms/api",
-  API_BASE_URL: "https://tfk9pkp6-3001.inc1.devtunnels.ms",
+  API_URL: "https://society-management-backend-v2.onrender.com/api",
+  API_BASE_URL: "https://society-management-backend-v2.onrender.com",
   REACT_APP_FRONTEND_URL: "http://localhost:3000",
 };
 

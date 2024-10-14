f381fddc2aec3aaebb1443701d19570b55fb1c55
Jewel Ghosh <jewel@Jewels-MacBook-Air.local>
2024-10-14 12:42:40 +0530

    testing ci/cd

--- src/components/NavBarForHome.tsx
+++ src/components/NavBarForHome.tsx
@@ -4,43 +4,48 @@ import DropdownForHome from "./DropdownForHome";
 const NavBarForHome = () => {
   const navigate = useNavigate();
   return (
-    <div className="shadow py-2 px-4 d-flex justify-content-between ">
-      <div className="side_navbar_header">
-        <a href="#">
-          <h3 style={{ color: "var(--cs-primary_bg_color)" }}>
-            <span className="gradient-text">Society Management</span>
-          </h3>
-        </a>
+    <>
+      <div className="shadow py-2 px-4 d-flex justify-content-between ">
+        <div className="side_navbar_header">
+          <a href="#">
+            <h3 style={{ color: "var(--cs-primary_bg_color)" }}>
+              <span className="gradient-text">Society Management</span>
+            </h3>
+          </a>
+        </div>
+        <div>
+          <h4
+            className="me-4 cursor_pointer"
+            onClick={() => navigate("/dashboard")}>
+            Dashboard
+          </h4>
+        </div>
+        <div className="auth_buttons_home d-flex">
+          <div className="sign_in me-3">
+            <button
+              className="btn_secondary"
+              onClick={() => navigate("/auth/sign-in")}
+            >
+              <span className="gradient-text">Sign In</span>
+            </button>
+          </div>
+          <div className="sign_up">
+            <button
+              className="btn_primary"
+              onClick={() => navigate("/auth/sign-up")}
+            >
+              <span>Sign Up</span>
+            </button>
+          </div>
+        </div>
+        <div className="auth_acc_dropdow">
+          <DropdownForHome />
+        </div>
       </div>
       <div>
-        <h4
-          className="me-4 cursor_pointer"
-          onClick={() => navigate("/dashboard")}>
-          Dashboard
-        </h4>
+        <h2 className="m-5 text-center">Jewel's society</h2>
       </div>
-      <div className="auth_buttons_home d-flex">
-        <div className="sign_in me-3">
-          <button
-            className="btn_secondary"
-            onClick={() => navigate("/auth/sign-in")}
-          >
-            <span className="gradient-text">Sign In</span>
-          </button>
-        </div>
-        <div className="sign_up">
-          <button
-            className="btn_primary"
-            onClick={() => navigate("/auth/sign-up")}
-          >
-            <span>Sign Up</span>
-          </button>
-        </div>
-      </div>
-      <div className="auth_acc_dropdow">
-        <DropdownForHome />
-      </div>
-    </div>
+    </>
   );
 };
 
